5b62c87d16c336315a0671231d8d8127114698c3
SValderrama <savalderrama@unal.edu.co>
2021-08-12 11:55:26 -0500

    Modo oscuro en el mapa - overlay sobre el buscador - Encuentra ubicacion

--- app/src/main/java/com/xuabux/XUABUXAPP/Overlay.kt
+++ app/src/main/java/com/xuabux/XUABUXAPP/Overlay.kt
@@ -1,38 +1,104 @@
 package com.xuabux.XUABUXAPP
 
 import android.graphics.Color
+import kotlin.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.data.geojson.GeoJsonLayer
 import com.xuabux.XUABUXAPP.R
 import org.json.JSONException
 import org.json.JSONObject
 import java.lang.StringBuilder
 import java.util.*
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import com.google.android.gms.maps.model.*
 
 class Overlay :  AppCompatActivity(), OnMapReadyCallback {
+    var PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    private lateinit var placesClient: PlacesClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var locationPermissionGranted = false
     private lateinit var mMap: GoogleMap
+    private var lastKnownLocation: Location? = null
+
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var likelyPlaceNames: Array<String?> = arrayOfNulls(0)
+    private var likelyPlaceAddresses: Array<String?> = arrayOfNulls(0)
+    private var likelyPlaceAttributions: Array<List<*>?> = arrayOfNulls(0)
+    private var likelyPlaceLatLngs: Array<LatLng?> = arrayOfNulls(0)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_overlay)
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        Places.initialize(applicationContext, "AIzaSyCcynBp2TY3JVT20q1K25a0VuRxxAfMc9k");
+        var placesClient = Places.createClient(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 
+
+    private fun getLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+        updateLocationUI()
+    }
     override fun onMapReady(googleMap: GoogleMap) {
+
         mMap = googleMap
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle));
+
         //mMap.isMyLocationEnabled = true;
         // Add a marker in Sydney and move the camera
         val nachoPos = LatLng(4.6381991,-74.0862351)
-        mMap.addMarker(MarkerOptions().position(nachoPos).title("Universidad Nacional De Colombia"))
-
         mMap.animateCamera(
             CameraUpdateFactory.newLatLngZoom(
                 LatLng(
@@ -53,7 +119,14 @@ class Overlay :  AppCompatActivity(), OnMapReadyCallback {
         val layer = GeoJsonLayer(mMap, js)
         layer.defaultLineStringStyle.color= Color.parseColor("#74EA56")
         layer.addLayerToMap()
+        getLocationPermission()
+        // [END_EXCLUDE]
 
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI()
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation()
 
 
 
@@ -68,5 +141,65 @@ class Overlay :  AppCompatActivity(), OnMapReadyCallback {
         }
         return null
     }
+    private fun updateLocationUI() {
+        if (mMap == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap?.isMyLocationEnabled = true
+                mMap?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                mMap?.isMyLocationEnabled = false
+                mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                        }
+                    } else {
+                        Log.d("XuaDebug", "Current location is null. Using defaults.")
+                        Log.e("XuaDebug", "Exception: %s", task.exception)
+                        mMap?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+    companion object {
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
 
+        // Keys for storing activity state.
+        // [START maps_current_place_state_keys]
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
+        // [END maps_current_place_state_keys]
+
+        // Used for selecting the current place.
+        private const val M_MAX_ENTRIES = 5
+    }
 }
